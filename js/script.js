725390ae30367e53c67568847e9630b102a6ea0e
Arisson Lima <arissonlima@gmail.com>
2023-09-14 22:44:38 -0300

    Created, styled and configured slide with thumbnails

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+var thumbnails = new Swiper(".slide-thumbnail", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  watchSlidesProgress: true
+});
+
 var slide = new Swiper(".slide-imagens", {
   effect: "fade",
   speed: 800,
-  grabCursor: true
+  grabCursor: true,
+  thumbs: {
+    swiper: thumbnails
+  }
 });
